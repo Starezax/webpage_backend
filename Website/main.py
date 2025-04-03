50d39c1c7c23f03241dfd3dfdb3ef2e90a2ae00c
Yulian Zaiats <yulianzaiats@gmail.com>
2025-04-03 19:00:21 +0300

    Fixed Everything, and made structure correct

--- Website/main.py
+++ Website/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
+
+# Main pages
 @app.route('/')
 def index():
     return render_template('main_page.html')
@@ -17,6 +19,34 @@ def map():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+# Wiki  subsections
+
+@app.route('/wiki_ww1')
+def wiki_ww1():
+    return render_template('wiki_ww1.html')
+
+@app.route('/wiki_ww2')
+def wiki_ww2():
+    return render_template('wiki_ww2.html')
+
+@app.route('/wiki_ussr')
+def wiki_ussr():
+    return render_template('wiki_ussr.html')
+
+@app.route('/wiki_1991')
+def wiki_1991():
+    return render_template('wiki_1991.html')
+
+@app.route('/wiki_2014')
+def wiki_2014():
+    return render_template('wiki_2014.html')
+
+@app.route('/wiki_2022')
+def wiki_2022():
+    return render_template('wiki_2022.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
