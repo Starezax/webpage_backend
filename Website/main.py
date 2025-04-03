69368b8d40e5dcd3a951ca4f545576f157d278e3
Yulian Zaiats <yulianzaiats@gmail.com>
2025-04-03 19:27:07 +0300

    Fixed button placement, added battles as json file

--- Website/main.py
+++ Website/main.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import folium
+import json
+import os
 
 app = Flask(__name__, template_folder='templates')
 
+def load_battles():
+    try:
+        with open('static/info/battles.json', 'r', encoding='utf-8') as f:
+            print("Opened")
+            return json.load(f)
+    except FileNotFoundError:
+        print('Not found')
+        return []
 
 # Main pages
 @app.route('/')
@@ -13,16 +24,32 @@ def wikipedia():
     return render_template('wikipedia.html')
 
 @app.route('/map')
-def map():
-    return render_template('map.html')
+def map_view():
+    m = folium.Map(location=[49.5, 31.5], zoom_start=6)
+
+    battles = load_battles()
+    if not battles:
+        print("Немає даних про битви в JSON файлі.")
+    
+    # Додаємо маркери на карту
+    for battle in battles:
+        if battle.get('latitude') and battle.get('longitude'):
+            folium.Marker(
+                location=[battle['latitude'], battle['longitude']],
+                popup=f"<strong>{battle['name']}</strong><br>Рік: {battle['year']}<br>{battle['info']}",
+                tooltip=battle['name']
+            ).add_to(m)
+        else:
+            print(f"Невірні координати для битви: {battle['name']}")
+
+    map_html = m._repr_html_()
+    return render_template('map.html', map_html=map_html)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-
-# Wiki  subsections
-
+# Wiki subsections
 @app.route('/wiki_ww1')
 def wiki_ww1():
     return render_template('wiki_ww1.html')
@@ -46,7 +73,6 @@ def wiki_2014():
 @app.route('/wiki_2022')
 def wiki_2022():
     return render_template('wiki_2022.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
